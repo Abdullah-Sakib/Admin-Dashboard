65fbd269825fbab6336e95d3f52b9d1e6e61cdb4
Abdullah Sakib <abdullahsakib888@gmail.com>
2024-06-16 21:00:23 +0600

    Completed the routing setup

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,8 +1,55 @@
+import { useContext } from "react";
+import { useTheme } from "@emotion/react";
+import { Box, IconButton, InputBase } from "@mui/material";
+import {
+  LightModeOutlined,
+  DarkModeOutlined,
+  NotificationsOutlined,
+  SettingsOutlined,
+  PersonOutlined,
+  Search,
+} from "@mui/icons-material";
+import { ColorModeContext, tokens } from "../../theme";
+
 const Topbar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+
   return (
-    <div>
-      <h1>Topbar</h1>
-    </div>
+    <Box display="flex" justifyContent="space-between" p={2}>
+      {/* SEARCH BAR */}
+      <Box
+        display="flex"
+        backgroundColor={colors.primary[400]}
+        borderRadius="3px"
+      >
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+        <IconButton type="button" p={1}>
+          <Search />
+        </IconButton>
+      </Box>
+
+      {/* ICONS */}
+      <Box display="flex">
+        <IconButton onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === "dark" ? (
+            <DarkModeOutlined />
+          ) : (
+            <LightModeOutlined />
+          )}
+        </IconButton>
+        <IconButton>
+          <NotificationsOutlined />
+        </IconButton>
+        <IconButton>
+          <SettingsOutlined />
+        </IconButton>
+        <IconButton>
+          <PersonOutlined />
+        </IconButton>
+      </Box>
+    </Box>
   );
 };
 
